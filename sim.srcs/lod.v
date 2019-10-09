1ebbadeab76e6c73baeaa7cdd582354ab1d55971
akhilrb <akhil.r.baranwal@gmail.com>
2019-10-09 14:57:41 +0530

    integrated encoder with LOD (FSM)

--- sim.srcs/lod.v
+++ sim.srcs/lod.v
@@ -23,22 +23,38 @@
 
 module lod #(parameter WIDTH = 8) (
     input [WIDTH-1:0] in,
-    output reg [$clog2(WIDTH):0] out
+    output reg [$clog2(WIDTH):0] leadingOneOut
 );
 
+// for implementing FSM for the priority encoder
+// tells the status of the encoder, whether the leading one is found 
+reg found;
+
 integer i;
-always @ (in)
+always @ (negedge found)
 begin
-    out = 0;
-    // also encode the data for shifting
-    for (i = WIDTH-1; i >= 0; i = i-1)
     begin
-        if (in[i])
+        for (i = WIDTH-1; i >= 0; i = i-1)
+        // proceed only if leading one not yet 'found'
+        if(found == 1'b0)
         begin
-            out = i;
-            break;
+            if (in[i])
+            begin
+                leadingOneOut = i;
+                $display("Found leading one at %d", i);
+                // Essentially need to put a break here, which won't synthesize
+                // which is precisely why an FSM is needed here.
+                found = 1'b1;
+            end
         end
     end
+end
+
+// change the value of 'found' once the input data changes
+always@(in)
+begin
+    leadingOneOut = 0;
+    found = 1'b0;
 end
 
 endmodule
